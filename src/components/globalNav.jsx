e204b1513733f7c436071d4477caa2bf0052803a
Ricky <rickyconnell@gmail.com>
2023-09-26 01:21:23 -0700

    Global Pages. Nav from frontmatter (#1)

---
+++ src/components/globalNav.jsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import { NavDropdown } from "react-bootstrap";
+
+// Controls the soring for the top of the navbar.
+// Update this to change what's across the navbar.
+// NOTE: Any headers not in this list will show up after it in alpha order
+// NOTE: Events will always be to the far right, unless changed in layout.jsx
+const navHead = ["General", "Contact", "How To"];
+
+function navSort(a, b) {
+   // we are comparing frontmatter, reduce for readability of logic
+   let left = a.frontmatter.nav;
+   let right = b.frontmatter.nav;
+
+   // Sorting Logic:
+   //   If the head is the same, return priority order.
+   //   If neither is in the headers list, return alphabetical order.
+   //   If only 1 isn't in the list, return 1/-1 to indicate which is already a know header.
+   //   If both are in the list, return the order from header array.
+   if (left.head === right.head) {
+      return left.priority - right.priority;
+   }
+   /* 
+      ToDo: If the heading is not in the header list alpha sort after the named headers
+    */
+   if (!navHead.includes(left.head) && !navHead.includes(right.head)) {
+      // neither head is in our list, return alpha order
+      return [left.head, right.head].sort();
+   } else if (!navHead.includes(left.head)) {
+      // left is not in our list, positive for right lesser
+      return 1;
+   } else if (!navHead.includes(right.head)) {
+      // right is not in our list, negative for left lesser
+      return -1;
+   }
+   // both are in our list, index math to the rescue
+   return navHead.indexOf(left.head) - navHead.indexOf(right.head);
+}
+
+const GlobalNav = () => {
+   const data = useStaticQuery(navQuery);
+   let headings = new Set(navHead);
+   let items = data.allMdx.nodes.sort(navSort);
+
+   items.forEach((item) => {
+      headings.add(item.frontmatter.nav.head);
+   });
+
+   return (
+      <>
+         {Array.from(headings).map((header) => {
+            return (
+               <NavDropdown title={header} id="basic-navbar-nav" key={header}>
+                  {items
+                     .filter((item) => {
+                        return header === item.frontmatter.nav.head;
+                     })
+                     .map((entry) => {
+                        return (
+                           <NavDropdown.Item key={header + entry.id}>
+                              <Link to="{entry.gatsbyPath">
+                                 {entry.frontmatter.nav.text}
+                              </Link>
+                           </NavDropdown.Item>
+                        );
+                     })}
+               </NavDropdown>
+            );
+         })}
+      </>
+   );
+};
+
+export const navQuery = graphql`
+   query navQuery {
+      allMdx(filter: { frontmatter: { globalPage: { ne: null } } }) {
+         nodes {
+            gatsbyPath(filePath: "/{mdx.frontmatter__globalPage}")
+            internal {
+               contentFilePath
+            }
+            frontmatter {
+               globalPage
+               nav {
+                  head
+                  priority
+                  text
+               }
+            }
+            id
+         }
+      }
+   }
+`;
+
+export default GlobalNav;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "gatsby";
+import GlobalNav from "./globalNav";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMastodon, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+   faFacebook,
+   faGithub,
+   faLinkedin,
+   faMastodon,
+   faSlideshare,
+   faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import bsidesLogo from "../images/bsides_logo.png";
 
@@ -12,23 +20,22 @@ const Layout = ({ children }) => {
    return (
       <Container>
          <Navbar expand="lg" className="bg-body-tertiary">
-            <Navbar.Brand href="/">
-               <img
-                  src={bsidesLogo}
-                  className="d-inline-block align-top"
-                  alt="Security BSides logo"
-               />
+            <Navbar.Brand>
+               <Link to="/">
+                  <img
+                     src={bsidesLogo}
+                     className="d-inline-block align-top"
+                     alt="Security BSides logo"
+                  />
+               </Link>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />{" "}
+            <Navbar.Toggle />
             <Navbar.Collapse id="basic-navbar-nav">
                <Nav className="me-auto">
-                  {/* <Nav.Link href="/">Home</Nav.Link> */}
-                  {/*  <Nav.Link href="#link">Link</Nav.Link> */}
-                  <NavDropdown title="Events" id="basic-nav-dropdown">
-                     <NavDropdown.Item href="/events">Map</NavDropdown.Item>
-                     <NavDropdown.Item href="#action/3.4">
-                        Upcoming
-                     </NavDropdown.Item>
+                  <GlobalNav />
+                  <NavDropdown title="Events">
+                     <NavDropdown.Item href="">Map</NavDropdown.Item>
+                     <NavDropdown.Item href="">Upcoming</NavDropdown.Item>
                      <NavDropdown.Divider />
                      <NavDropdown.Item>
                         <Link to={"/events/"}>The Full List</Link>
@@ -37,14 +44,23 @@ const Layout = ({ children }) => {
                </Nav>
             </Navbar.Collapse>
             <Nav>
-               <Nav.Link href="mailto:info@securitybsides.org">
-                  <FontAwesomeIcon icon={faEnvelope} size="xl" />
-               </Nav.Link>
                <Nav.Link href="https://infosec.exchange/@SecurityBSidesGlobal">
                   <FontAwesomeIcon icon={faMastodon} size="xl" />
                </Nav.Link>
+               <Nav.Link href="https://github.com/securitybsides">
+                  <FontAwesomeIcon icon={faGithub} size="xl" />
+               </Nav.Link>
                <Nav.Link href="https://twitter.com/securitybsides">
                   <FontAwesomeIcon icon={faTwitter} size="xl" />
+               </Nav.Link>
+               <Nav.Link href="https://www.facebook.com/groups/232453505462/">
+                  <FontAwesomeIcon icon={faFacebook} size="xl" />
+               </Nav.Link>
+               <Nav.Link href="https://www.linkedin.com/groups/2178344/">
+                  <FontAwesomeIcon icon={faLinkedin} size="xl" />
+               </Nav.Link>
+               <Nav.Link href="https://www.slideshare.net/BSides">
+                  <FontAwesomeIcon icon={faSlideshare} size="xl" />
                </Nav.Link>
             </Nav>
          </Navbar>
