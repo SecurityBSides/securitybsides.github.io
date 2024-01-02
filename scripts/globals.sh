9a32934638af9adcfaecc892ce609b9384b590fa
rlc4 <rickyconnell@gmail.com>
2024-01-02 02:18:20 -0800

    Conversion script that works for 95%

---
+++ scripts/globals.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/contact\ update\ form.html src/global/contact\ update\ form.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/FrontPage.html src/global/welcome.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/BackChannel.html src/global/BackChannel.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/BSidesHistory.html src/global/BSidesHistory.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/BSides\ Stories.html src/global/BSidesStories.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/EventHistory.html src/global/EventHistory.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/Event\ Quick\ Start\ Guide.html src/global/EventQuickStartGuide.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/EventRoles.html src/global/EventRoles.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/FAQ.html src/global/FAQ.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/Groups\ and\ Social\ Media\ page.html src/global/GroupsAndSocial.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/Guidelines.html src/global/Guidelines.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/HowToGetStarted.html src/global/HowToGetStarted.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/InfoSecAndTheLaw.html src/global/InfoSecAndTheLaw.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/Media.html src/global/Media.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/OrganizeALocalBSides.html src/global/OrganizeALocalBSides.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/SideBar.html src/global/SideBar.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/What\ BSides\ Means.html src/global/WhatBsidesMeans.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/BSides.html src/global/WhatIsBSides.mdx 
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/SiteMaintainers.html src/global/SiteMaintainers.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/SponsorPitch.html src/global/SponsorPitch.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/Sponsoring.html src/global/Sponsoring.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/Twitter\ and\ Mastodon\ Lists.html src/global/twitterAndMastadon.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/UsingTheBSidesWiki.html src/global/UsingTheBSidesWiki.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/What\ BSides\ Means.html src/global/WhatBSidesMeans.mdx
+node ./scripts/convertGlobal.mjs ../securitybsides-archive/WhatToExpect.html src/global/WhatToExpect.mdx
+
